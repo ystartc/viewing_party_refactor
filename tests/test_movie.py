23c234a4c0f9cc22a14de1f8ac29cf97135f0cc0
Angie <caritocon@gmail.com>
2023-03-31 11:53:26 -0700

    added watchlist instance to class movie and run the tests- create

--- tests/test_movie.py
+++ tests/test_movie.py
@@ -9,3 +9,15 @@ def test_creating_movie_initializes_instance_variables():
     assert movie.name == 'Titanic'
     assert movie.genre == 'Romance'
     assert movie.rating == 10
+
+def test_adding_movie_multiple_times_does_not_create_duplicate():
+    # Arrange
+    movie = Movie("Titanic", "Romance", 10)
+    movie2 = Movie("Redemption", "Action", 8)
+
+    # Act
+    movie.add_movie_to_watchlist(movie2)
+    movie.add_movie_to_watchlist(movie2)
+
+    # Assert
+    assert movie.watchlist == [movie2]
--- viewing_party/movie.py
+++ viewing_party/movie.py
@@ -3,5 +3,10 @@ class Movie:
         self.name = name
         self.genre = genre
         self.rating = rating
+        self.watchlist = []
+
+    def add_movie_to_watchlist(self, new_movie):
+        if new_movie not in self.watchlist:
+            self.watchlist.append(new_movie)
     
     
