1e46ff9a0045c3823da7e622e4369fe8727a45a2
Auberon López <auberonLopez@gmail.com>
2023-03-31 11:15:04 -0700

    Added name and friend functionality to Person

--- tests/test_person.py
+++ tests/test_person.py
@@ -1,9 +1,23 @@
+from viewing_party.person import Person
+
 import pytest
 
-def test_1():
-    # Arrange
-
-    # Act
+def test_creating_person_initializes_instance_variables():
+    # Arrange / Act
+    kendall = Person("Kendall")
 
     # Assert
-    pass
+    assert kendall.name == "Kendall"
+    assert kendall.friends == []
+
+def test_adding_friend_multiple_times_does_not_create_duplicate():
+    # Arrange
+    kendall = Person("Kendall")
+    simon = Person("Simon")
+
+    # Act
+    kendall.add_friend(simon)
+    kendall.add_friend(simon)
+
+    # Assert
+    assert kendall.friends == [simon]
--- viewing_party/person.py
+++ viewing_party/person.py
@@ -1,3 +1,9 @@
 class Person:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.friends = []
+
+    def add_friend(self, new_friend):
+        if new_friend not in self.friends:
+            self.friends.append(new_friend)
     
