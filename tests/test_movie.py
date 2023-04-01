4cb4d8365063f711a0732d75898c3387135a80d9
Yulia S <startyupp@gmail.com>
2023-03-31 20:33:25 -0700

    .

--- tests/test_movie.py
+++ tests/test_movie.py
@@ -1,6 +1,5 @@
 import pytest
 from viewing_party.movie import Movie
-from viewing_party.person import Person
 
 def test_creating_movie_initializes_instance_variables():
     # Arrange/Act
@@ -11,15 +10,3 @@ def test_creating_movie_initializes_instance_variables():
     assert movie.genre == 'Romance'
     assert movie.rating == 10
 
-def test_adding_movie_multiple_times_does_not_create_duplicate():
-    # Arrange
-    
-    movie = Movie("Redemption", "Action", 8)
-    person = Person('Angie')
-    
-    # Act
-    person.add_movie_to_watchlist(movie)
-    person.add_movie_to_watchlist(movie)
-
-    # Assert
-    assert movie.watchlist == [movie]
